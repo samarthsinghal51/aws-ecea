64ffefd86977d6505ebab59a81b318c98e8f68c8
samarthsinghal51 <udemy5104@gmail.com>
2021-02-25 23:38:57 +0530

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 8000;
+const PORT = 8080;
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
